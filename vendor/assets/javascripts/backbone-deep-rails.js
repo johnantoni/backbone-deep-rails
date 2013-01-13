ff104d128b2c3903a0038b95e3bc3867a2512e72
John Griffiths <john@johnantoni.com>
2013-01-13 15:34:03 -0500

    asset path wrong

--- vendor/assets/javascripts/backbone-deep-rails.js
+++ vendor/assets/javascripts/backbone-deep-rails.js
@@ -1,2 +1,2 @@
-//= require backbone-deep-model-rails/lib/underscore.mixin.deepExtend.js
-//= require backbone-deep-model-rails/deep-model.js
+//= require backbone-deep-rails/lib/underscore.mixin.deepExtend.js
+//= require backbone-deep-rails/deep-model.js
